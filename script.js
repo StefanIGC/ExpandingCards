0db66d59bf89219847ec44d7564ac5d449038117
Stefan Chiciuc <stefanchiciuc@gmail.com>
2023-05-09 22:22:18 +0300

    Add auto-chnage every 5 seconds

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let panelsElement = document.querySelectorAll('.panel');
+let activePanelIndex = 0;
+const intervalTime = 5000; // 10 seconds
 
 let removeActiveClasses = () => {
     panelsElement.forEach(panel => {
@@ -6,19 +8,25 @@ let removeActiveClasses = () => {
     });
 };
 
-let activatePanel = (panel) => {
+let activatePanel = () => {
     removeActiveClasses();
-    panel.classList.add('active');
+    panelsElement[activePanelIndex].classList.add('active');
+    activePanelIndex = (activePanelIndex + 1) % panelsElement.length;
 };
 
 panelsElement.forEach(panel => {
     panel.addEventListener('click', () => {
-        activatePanel(panel);
+        activePanelIndex = Array.from(panelsElement).indexOf(panel);
+        activatePanel();
     });
+    
     panel.addEventListener('mouseover', () => {
-        activatePanel(panel);
+        activePanelIndex = Array.from(panelsElement).indexOf(panel);
+        activatePanel();
     });
 });
+
+setInterval(activatePanel, intervalTime);
 
 
 
